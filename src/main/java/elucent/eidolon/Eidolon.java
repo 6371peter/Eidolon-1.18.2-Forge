9325ab44642cda93845dd870aa2a706eaa931216
6371peter <peterli6371@gmail.com>
2023-11-08 07:45:28 +0800

    updata.

--- src/main/java/elucent/eidolon/Eidolon.java
+++ src/main/java/elucent/eidolon/Eidolon.java
@@ -23,6 +23,7 @@ import elucent.eidolon.tile.NecroticFocusTileRenderer;
 import elucent.eidolon.tile.SoulEnchanterTileRenderer;
 import elucent.eidolon.tile.reagent.CisternTileRenderer;
 import elucent.eidolon.tile.reagent.PipeTileRenderer;
+// import elucent.eidolon.world.WorldGen;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
@@ -86,6 +87,14 @@ public class Eidolon {
         FMLJavaModLoadingContext.get().getModEventBus().register(new Registry());
         Registry.init();
         proxy.init();
+
+        /*
+        MinecraftForge.EVENT_BUS.register(new WorldGen());
+        WorldGen.preInit();
+        WorldGen.init();
+        FMLJavaModLoadingContext.get().getModEventBus().register(new WorldGen());
+         */
+
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new Events());
         DistExecutor.unsafeCallWhenOn(Dist.CLIENT, () -> () -> {
